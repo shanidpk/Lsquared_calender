6af5c39163ee2d1d327cc5a4d2901d0de9639c72
shanid01 <shanid.ameen@aethanalytica.com>
2025-05-17 11:24:29 +0530

    home

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+  goToDashboard() {
+    alert('Go to Dashboard');
+  }
+  
+}
